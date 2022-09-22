7a504939dacec22a00685c98b6f7a960011270c0
Gatito - ネコ <84881966+Gatito-kw@users.noreply.github.com>
2022-09-22 10:51:35 -0500

    Update config.js

--- config.js
+++ config.js
@@ -22,6 +22,16 @@ global.sessionName = 'hisoka'
 global.prefa = ['', '!', '+', '#', '.']
 
 
+global.mess = (type, m, neko) => {
+    let msg = {
+        owner: 'Este comando solo puede ser ejecutado por el *creador* de la bot!',
+        group: 'Este comando solo puede ser ejecutado en *grupos*!',
+        admin: 'Esta función solo puede ser ejecutado por los *administradores\'as* del grupo!',
+        botadmin: 'La bot debe ser *administradora* para ejecutar esta función!',
+    }[type]
+    if (msg) return m.reply(msg)
+}
+
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
 	fs.unwatchFile(file)
